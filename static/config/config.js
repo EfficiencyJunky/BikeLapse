343a64e765c89d02bce27c279d7f936d0d94fba8
Turner Kirk <voxpipes@gmail.com>
2020-07-10 10:52:44 -0700

    added publicly accessible API_Key

--- static/config/config.js
+++ static/config/config.js
@@ -1,2 +1,4 @@
 // API key
-const API_KEY = "insert_mapbox_api_key_here";
+// const API_KEY = "insert_mapbox_api_key_here";
+
+const API_KEY = "pk.eyJ1IjoibWFwc3R5bGUxMCIsImEiOiJja2NnaW9sOHcwN3NuMnlsaWZnNTMzc3U2In0.WiJ7HoKqFAnQaW-JoallJg";
