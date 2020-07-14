692f56920b75febcca99cc15951c015d34a7a876
EfficiencyJunky <runner@fv-az60.xqka3zcluhuupn4vzlax1z3v5h.cx.internal.cloudapp.net>
2020-07-14 05:30:10 +0000

    Automated commit - update all_ride_file_names.js in the /static/config folder

--- static/config/all_ride_file_names.js
+++ static/config/all_ride_file_names.js
@@ -1,1 +1,1 @@
-let bikeRideJSONFileNames = ['2020_06_21--crystalsprings.json', '2020_07_05--rodeobeach.json', '2020_07_11--ingleside.json', '2020_07_15--paradiseloop.json']
+let bikeRideJSONFileNames = ['2020_06_21--crystalsprings.json', '2020_07_05--rodeobeach.json', '2020_07_15--paradiseloop.json']
